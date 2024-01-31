9f28098048a5e8631ab642629ca4e49fa6f7e8ab
aslasusa <aslasusa@gmail.com>
2024-01-31 10:07:15 -0500

    Init Commit & getSum method

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static int getSum(int a, int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        System.out.println(getSum(5,3));
+    }
+
+
+
+
+}
