18627d1f3ccd51173fa90485dc77049daaf7859b
aslasusa <aslasusa@gmail.com>
2024-01-31 10:10:02 -0500

    Added isEight Boolean

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,13 @@ public class Main {
         System.out.println("Hello world!");
 
         System.out.println(getSum(5,3));
+        System.out.println(isEight(getSum(5,3)));
+        System.out.println(isEight(getSum(6,3)));
     }
 
+    public static boolean isEight(int num){
+        return num ==8;
+    }
 
 
 
